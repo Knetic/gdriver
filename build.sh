84fbf22ec3c4a4bf6e5ec3eda3fae9ab94cf474e
George Lester <glester491@gmail.com>
2015-06-08 10:25:40 -0700

    Added packaging support to build script

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 export GOPATH=$(pwd)
 
+echo "Getting dependencies"
 go get code.google.com/p/google-api-go-client/drive/v2
 go get code.google.com/p/goauth2/oauth
 
+echo "Compiling"
 mkdir -p output
 
 # If you're building from windows, don't forget to install the bootstrappers:
@@ -13,11 +15,36 @@ mkdir -p output
 
 export GOARCH=amd64
 
+echo "Building windows"
 export GOOS=windows
 go build -o output/gdriver.exe .
 
+echo "Building darwin"
 export GOOS=darwin
 go build -o output/gdriver.bin .
 
+echo "Building linux"
 export GOOS=linux
 go build -o output/gdriver .
+
+echo "Packaging"
+pushd output > /dev/null
+
+rm ./*.rpm
+rm ./*.deb
+
+fpm -n gdriver \
+-s dir \
+-t rpm \
+-v 1.0 \
+--log error \
+./gdriver=/usr/local/bin/gdriver
+
+fpm -n gdriver \
+-s dir \
+-t deb \
+-v 1.0 \
+--log error \
+./gdriver=/usr/local/bin/gdriver
+
+popd > /dev/null
