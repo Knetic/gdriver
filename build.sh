4fa4df1329b2cb2eeece890bbb4d4d7f3295c566
George Lester <glester491@gmail.com>
2015-06-07 19:11:08 -0700

    Added cross-platform build

--- build.sh
+++ build.sh
@@ -6,4 +6,18 @@ go get code.google.com/p/google-api-go-client/drive/v2
 go get code.google.com/p/goauth2/oauth
 
 mkdir -p output
+
+# If you're building from windows, don't forget to install the bootstrappers:
+#go tool dist install -v pkg/runtime
+#go install -v -a std
+
+export GOARCH=amd64
+
+export GOOS=windows
 go build -o output/gdriver.exe .
+
+export GOOS=darwin
+go build -o output/gdriver.bin .
+
+export GOOS=linux
+go build -o output/gdriver .
